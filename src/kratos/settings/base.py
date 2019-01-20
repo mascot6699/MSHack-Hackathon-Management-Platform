12cf88904432038f867843b1134e9c7ac8093999
mascot66993 <umang.shukla@coinswitch.co>
2019-01-20 06:13:53 +0530

    Adds AmbassadorInfoForm

--- src/kratos/settings/base.py
+++ src/kratos/settings/base.py
@@ -77,6 +77,7 @@ INSTALLED_APPS = (
     'easy_thumbnails',
 
     'profiles',
+    'kratos',
     'accounts',
     'rest_framework',
 
--- src/kratos/views.py
+++ src/kratos/views.py
@@ -1,4 +1,7 @@
+from django.shortcuts import render
 from django.views import generic
+
+from .forms import AmbassadorInfoForm
 
 
 class HomePage(generic.TemplateView):
@@ -8,9 +11,15 @@ class HomePage(generic.TemplateView):
 class Chat(generic.TemplateView):
     template_name = "index.html"
 
+# Deprecated
+# class ApplyPage(generic.TemplateView):
+#     template_name = "apply.html"
+#
 
-class ApplyPage(generic.TemplateView):
-    template_name = "apply.html"
+def apply_new(request):
+    form = AmbassadorInfoForm()
+    return render(request, 'apply.html', {'form': form})
+
 
 class AboutPage(generic.TemplateView):
     template_name = "about.html"
